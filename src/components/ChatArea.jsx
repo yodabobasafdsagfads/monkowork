71a2ce050e0eb5457363fb4c4488f2f7f79686bd
yodabobasafdsagfads <bobayoda2@gmail.com>
2025-10-26 17:05:56 +0100

    Delete src/components/ChatArea.jsx

--- src/components/ChatArea.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function ChatArea({ user, server, dm }){
-  return (
-    <div className="chat-area">
-      {dm ? <h2>DM with {dm.displayName||dm.email}</h2> : <h2>{server?server.name:'Select a DM or server'}</h2>}
-    </div>
-  );
-}
